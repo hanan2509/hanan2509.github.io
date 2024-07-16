f265243939a83bb97d89f4db95934f1832413f18
hanan2509 <hananprianto25@gmail.com>
2024-07-16 10:25:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function saklar(params) {
       console.log(params);
       let lampu1 = document.getElementById("lampu1");
       if(params == "on"){
-        lampu1.src = "/assets/on.gif";
+        lampu1.src = "on.gif";
       }
       if(params == "off"){
-        lampu1.src = "/assets/off.gif";
+        lampu1.src = "off.gif";
       }
       return lampu1;
 }
 var on = new Audio();
-  on.src = "sounds/on.mp3";
+  on.src = "on.mp3";
       
 var off = new Audio();
-  off.src = "sounds/off.mp3"
+  off.src = "off.mp3"
